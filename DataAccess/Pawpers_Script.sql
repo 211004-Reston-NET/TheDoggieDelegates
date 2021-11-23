3367f458b78f89b4fc9f5e1cbbecaf82c92b7410
jerid92 <33670855+jerid92@users.noreply.github.com>
2021-11-23 13:12:19 -0600

    Created State Join Table

--- DataAccess/Pawpers_Script.sql
+++ DataAccess/Pawpers_Script.sql
@@ -31,8 +31,21 @@ Create Table Profile
 	
 	CONSTRAINT pro_dwe_FK FOREIGN KEY(profile_dwellingid)
 			REFERENCES Dwelling(dwelling_id),
+			
+	CONSTRAINT pro_sta_FK FOREIGN KEY(profile_state)
+			REFERENCES State(state_id),
 	
 );
+
+ALTER TABLE Profile 
+ADD CONSTRAINT pro_sta_FK 
+FOREIGN KEY(profile_stateid) REFERENCES State(state_id)
+
+ALTER TABLE Profile
+DROP COLUMN profile_state
+
+ALTER TABLE Profile
+ADD profile_stateid INT NOT NULL
 
 Create Table Dwelling
 (
@@ -40,3 +53,69 @@ Create Table Dwelling
 	dwelling_type             VARCHAR(32) NOT NULL,
 
 );
+
+CREATE TABLE [state](
+  
+[state_id] [int] IDENTITY(1,1) NOT NULL,
+[state_code] [nchar](2) NOT NULL,
+[state_name] [nvarchar](128) NOT NULL,
+
+CONSTRAINT [PK_state] PRIMARY KEY CLUSTERED 
+(  [state_id] ASC )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]) 
+ON [PRIMARY]
+
+
+
+INSERT into [state] values ('AL', 'Alabama'),
+('AK', 'Alaska'),
+('AL', 'Alabama'),
+('AZ', 'Arizona'),
+('AR', 'Arkansas'),
+('CA', 'California'),
+('CO', 'Colorado'),
+('CT', 'Connecticut'),
+('DE', 'Delaware'),
+('DC', 'District of Columbia'),
+('FL', 'Florida'),
+('GA', 'Georgia'),
+('HI', 'Hawaii'),
+('ID', 'Idaho'),
+('IL', 'Illinois'),
+('IN', 'Indiana'),
+('IA', 'Iowa'),
+('KS', 'Kansas'),
+('KY', 'Kentucky'),
+('LA', 'Louisiana'),
+('ME', 'Maine'),
+('MD', 'Maryland'),
+('MA', 'Massachusetts'),
+('MI', 'Michigan'),
+('MN', 'Minnesota'),
+('MS', 'Mississippi'),
+('MO', 'Missouri'),
+('MT', 'Montana'),
+('NE', 'Nebraska'),
+('NV', 'Nevada'),
+('NH', 'New Hampshire'),
+('NJ', 'New Jersey'),
+('NM', 'New Mexico'),
+('NY', 'New York'),
+('NC', 'North Carolina'),
+('ND', 'North Dakota'),
+('OH', 'Ohio'),
+('OK', 'Oklahoma'),
+('OR', 'Oregon'),
+('PA', 'Pennsylvania'),
+('PR', 'Puerto Rico'),
+('RI', 'Rhode Island'),
+('SC', 'South Carolina'),
+('SD', 'South Dakota'),
+('TN', 'Tennessee'),
+('TX', 'Texas'),
+('UT', 'Utah'),
+('VT', 'Vermont'),
+('VA', 'Virginia'),
+('WA', 'Washington'),
+('WV', 'West Virginia'),
+('WI', 'Wisconsin'),
+('WY', 'Wyoming');
