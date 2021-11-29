dd3ff9daa346cd93fb4cabb15ea11f8ede4b8a23
endochroma <mlm@michael-mason.com>
2021-11-29 09:52:26 -0700

    login

--- pawpers-angular/src/app/app.module.ts
+++ pawpers-angular/src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { Auth0LoginComponent } from './auth0-login/auth0-login.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, Auth0LoginComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{path: '', component:FrontPageComponent}]),
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-tc6s9x9t.us.auth0.com',
+      clientId: 'xRoxK7dHIK8DecyAzJsMlTfUEcLH9mdC'
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
