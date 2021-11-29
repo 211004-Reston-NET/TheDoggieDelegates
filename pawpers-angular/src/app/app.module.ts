0a08fefdece9e86690981b9b9265a1fbeb6f8b52
AnddyLopez <anddylopez5324@gmail.com>
2021-11-29 16:23:11 -0600

    fixing merge conflicts?

--- pawpers-angular/src/app/app.module.ts
+++ pawpers-angular/src/app/app.module.ts
@@ -2,26 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainWeatherComponent } from './main-weather/main-weather.component';
-import { FrontPageComponent } from './front-page/front-page.component';
-import { RouterModule } from '@angular/router';
-import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainWeatherComponent,
-    FrontPageComponent,
-    ProfilePageComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: FrontPageComponent},
-      { path: 'profile-page', component: ProfilePageComponent},
-      { path: 'main-weather', component: MainWeatherComponent},
-      { path: '**', component:FrontPageComponent}
-    ])
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
